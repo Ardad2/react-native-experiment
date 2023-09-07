b18a14892214bf7605127bd93243f3f0e7438152
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-09-06 21:41:24 -0700

    added stack navigation

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
-import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import SignIn from './Screens/SignIn';
+import SignUp from './Screens/SignUp';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.mainView}>
-      <Text>Hello World</Text>
-      <Button title="click me"/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="signIn" component={SignIn}/>
+        <Stack.Screen name="signUp" component={SignUp}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
